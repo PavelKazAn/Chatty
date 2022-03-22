232d231932ead42855f5826c60d2293f85781cd2
PavelKazAn <frozzty@yandex.ru>
2022-03-22 19:29:40 +0300

    Initial commit

---
+++ client/src/main/java/client/WriteChatHistory.java
@@ -0,0 +1,11 @@
+package client;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class WriteChatHistory {
+    private String login;
+    private FileInputStream inputStream;
+    private FileOutputStream outputStream;
+
+}
